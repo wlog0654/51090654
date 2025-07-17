0c230a78703c867b7b280f994b90f52d9d0d494f
wlog0654 <wlog0654@gmail.com>
2025-07-18 01:59:21 +0800

    7.18 2.00am

--- src/content.config.ts
+++ src/content.config.ts
@@ -6,13 +6,13 @@ const blog = defineCollection({
 	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
 	// Type-check frontmatter using a schema
 	schema: ({ image }) => z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: image().optional(),
-	}),
+    	title: z.string().optional(),
+    	description: z.string().optional(),
+    	// Transform string to Date object
+    	pubDate: z.coerce.date().optional(), // <--- 把它设为可选
+    	updatedDate: z.coerce.date().optional(),
+    	heroImage: image().optional(),
+}),
 });
 
 export const collections = { blog };
